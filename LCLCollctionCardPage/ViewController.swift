7f92e61aea70994fdf2512488957d9d4d149316d
LinChl <lcl3@meitu.com>
2018-11-07 14:47:34 +0800

    [feature]first commit

---
+++ LCLCollctionCardPage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LCLCollctionCardPage
+//
+//  Created by linchl on 2018/11/7.
+//  Copyright © 2018年 linchl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ LCLCollctionCardPageTests/LCLCollctionCardPageTests.swift
@@ -0,0 +1,34 @@
+//
+//  LCLCollctionCardPageTests.swift
+//  LCLCollctionCardPageTests
+//
+//  Created by linchl on 2018/11/7.
+//  Copyright © 2018年 linchl. All rights reserved.
+//
+
+import XCTest
+@testable import LCLCollctionCardPage
+
+class LCLCollctionCardPageTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
